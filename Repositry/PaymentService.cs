bd38a3704c226ef0beb7e8afed9ff2b2221596d5
Md-Gosoddin <Md.Gosoddin@gmail.com>
2026-03-08 16:52:50 +0530

    Factory Pattern With DI Completed

---
+++ Repositry/PaymentService.cs
@@ -0,0 +1,57 @@
+﻿namespace KeyedServiceExample_With_DI.Repositry
+{
+
+
+    public enum PaymentOption
+    {
+        CreditCard,
+        Debitcard,
+        UPIPayement,
+        Paypal
+    }
+    public interface IPaymentService
+    {
+        void Paid();
+        PaymentOption PaymentOption { get; }
+    }
+
+
+    public class CreditCard : IPaymentService
+    {
+        public PaymentOption PaymentOption => PaymentOption.CreditCard;
+
+        public void Paid()
+        {
+            Console.WriteLine($"Payment Typeof : {PaymentOption}");
+        }
+    }
+
+    public class DebitCard : IPaymentService
+    {
+        public PaymentOption PaymentOption => PaymentOption.Debitcard;
+
+        public void Paid()
+        {
+            Console.WriteLine($"Payment Typeof : {PaymentOption}");
+        }
+    }
+
+    public interface IPaymentFactory
+    {
+        IPaymentService paymentservice(PaymentOption paymentOption);
+    }
+    public class PaymentFactory : IPaymentFactory
+    {
+        private readonly IEnumerable<IPaymentService> OpaymentServices;
+        public PaymentFactory(IEnumerable<IPaymentService> _paymentServices)
+        {
+            OpaymentServices = _paymentServices;
+        }
+        public IPaymentService paymentservice(PaymentOption paymentOption)
+        {
+            var findtype = OpaymentServices.FirstOrDefault(x => x.PaymentOption == paymentOption);
+            return findtype ?? throw new NotImplementedException($"Payment type of {paymentOption} is not Found");
+        }
+    }
+
+}
