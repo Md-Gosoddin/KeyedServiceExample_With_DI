43b9ac5cefde3ed46d723d5d28ad9494c64f7a62
Md-Gosoddin <Md.Gosoddin@gmail.com>
2026-03-08 17:18:53 +0530

    Autoservice Discovery or Assembly Scanning Completed

---
+++ MiddleWare/IEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace KeyedServiceExample_With_DI.MiddleWare
+{
+    public interface IEndpoint
+    {
+        void MapEndPoint(IEndpointRouteBuilder app);
+    }
+
+    public static class MapEndPoint
+    {
+
+        public static void MapAllEndpoints(this IEndpointRouteBuilder app)
+        {
+            //!t.IsInterface	Exclude the Interface itself.
+            //!t.IsAbstract	Exclude Incomplete Classes that can't be created.
+            var endpointDiscovery = typeof(IEndpoint).Assembly.GetTypes()
+                .Where(t => typeof(IEndpoint).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+            foreach (var type in endpointDiscovery)
+            {
+                var endpoint = (IEndpoint)Activator.CreateInstance(type)!;
+                endpoint.MapEndPoint(app);
+            }
+        }
+    }
+}
