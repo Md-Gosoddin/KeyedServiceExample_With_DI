0ec3c5b259032e12019f1343c68c195ca09ae0d0
Md-Gosoddin <Md.Gosoddin@gmail.com>
2026-03-08 17:50:32 +0530

    CreditCard, DebitCard Service Registerd

---
+++ Presentation/CreditCardEndPoint.cs
@@ -0,0 +1,17 @@
+﻿using KeyedServiceExample_With_DI.MiddleWare;
+using KeyedServiceExample_With_DI.Repositry;
+
+namespace KeyedServiceExample_With_DI.Presentation
+{
+    public class CreditCardEndPoint : IEndpoint
+    {
+        public void MapEndPoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/Creditcard", async ([FromKeyedServices("PaymentSystem")] IPaymentFactory PaymentFactory) =>
+            {
+                PaymentFactory.paymentservice(PaymentOption.CreditCard);
+                Console.WriteLine($"Credit-Card service");
+            });
+        }
+    }
+}
---
+++ Presentation/DebitcardService.cs
@@ -0,0 +1,15 @@
+﻿using KeyedServiceExample_With_DI.MiddleWare;
+
+namespace KeyedServiceExample_With_DI.Presentation
+{
+    public class DebitcardService : IEndpoint
+    {
+        public void MapEndPoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/DebitCard", async () =>
+            {
+                Console.WriteLine("Debitcard Service");
+            });
+        }
+    }
+}
